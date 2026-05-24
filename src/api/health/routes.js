78c1b3e90cc99c79788451e528c1139cce40020f
fadilfasa <fadilfasa03@gmail.com>
2026-05-25 06:36:04 +0800

    update health

--- src/api/health/routes.js
+++ src/api/health/routes.js
@@ -2,13 +2,14 @@ const routes = () => [
   {
     method: 'GET',
     path: '/health',
-    handler: (request, h) => h
-        .response({
-          status: 'success',
-          message: 'Server is running and healthy!',
-          timestamp: new Date().toISOString(),
-        })
-        .code(200),
+    handler: (request, h) => 
+    h
+      .response({
+        status: 'success',
+        message: 'Server is running and healthy!',
+        timestamp: new Date().toISOString(),
+      })
+      .code(200),
   },
 ];
 module.exports = routes;
